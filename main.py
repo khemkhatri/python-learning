d0d2bdf7c4cb04be12200d5dbfc575b9677b7aee
khemkhatri <khatri.khemm@gmail.com>
2025-02-08 10:35:43 +0545

    lbs to kg

--- main.py
+++ main.py
@@ -29,3 +29,12 @@ print(feet_value)
 
 print ("goodnight")
 print ("hello")
+
+
+def conversion_kg_lbs(kg):
+    lbs = kg *  2.20462
+    return lbs 
+
+kg = float(input("enter weight into kilograms: "))
+pounds = conversion_kg_lbs(kg)
+print(f"{kg} kg is equal to {pounds} lbs")
--- part2.py
+++ part2.py
@@ -1,2 +1,9 @@
-price = 41
-print ( not price >52 )
+
+
+def conversion_kg_lbs(kg):
+    lbs = kg *  2.20462
+    return lbs 
+
+kg = float(input("enter weight into kilograms: "))
+pounds = conversion_kg_lbs(kg)
+print(f"{kg} kg is equal to {pounds} lbs")
